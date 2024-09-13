39b97dc9ed7d90c74aa84c560d536e485e942429
HP <premrajmoze@gmail.com>
2024-09-13 23:09:43 +0530

    new add

---
+++ src/test/java/newDemo.java
@@ -0,0 +1,12 @@
+public class newDemo {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b=30;
+
+        int c=a+b;
+        int d=a-b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
