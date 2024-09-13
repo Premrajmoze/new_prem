6b8f42e86d206dd9520af1d47b92423fa76ac0e2
HP <premrajmoze@gmail.com>
2024-09-13 23:45:36 +0530

    2 time add

---
+++ src/test/java/newdemo1.java
@@ -0,0 +1,11 @@
+public class newdemo1 {
+    public static void main(String[] args) {
+        int a=10;
+        String str = "my name is prem";
+        String str1 = "i am prem";
+
+        System.out.println(a +":"+str);
+        System.out.println(str1);
+
+    }
+}
